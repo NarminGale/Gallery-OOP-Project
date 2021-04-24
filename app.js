57c0397b0b76388930c35169dc32fcd41f2242a7
NarminGale <narmingale@gmail.com>
2021-04-24 18:14:07 +0400

    Selected images

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   )
 }
+
+function Gallery(element) {
+  this.list = [...element.querySelectorAll('.img')]
+  // target
+  this.modal = getElement('.modal')
+  this.modalImg = getElement('.main-img')
+  this.modalImages = getElement('.modal-images')
+  this.closeBtn = getElement('.close-btn')
+  this.nextBtn = getElement('.next-btn')
+  this.prevBtn = getElement('.prev-btn')
+  console.log(this.modalImages)
+}
+
+const flower = new Gallery(getElement('.flowers'))
+const home = new Gallery(getElement('.home'))
